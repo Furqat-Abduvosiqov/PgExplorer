ff36806c160741e0cf39d90645e93907507d2d16
Furqat-Abduvosiqov <furqata93@gmail.com>
2025-09-10 12:52:27 +0500

    feat: add encryption service in shared project

---
+++ src/backend/Pg.Explorer.Shared/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Pg.Explorer.Shared.Encryptions;
+
+namespace Pg.Explorer.Shared;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddUtilities(this IServiceCollection services)
+    {
+        services.AddScoped<IEncryptionService, EncryptionService>();
+        return services;
+    }
+}
