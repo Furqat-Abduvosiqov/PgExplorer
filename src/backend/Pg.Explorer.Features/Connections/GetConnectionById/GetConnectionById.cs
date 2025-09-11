86f85ffff28a46c6655e5a41c76d4b4939eb28e1
Furqat-Abduvosiqov <furqata93@gmail.com>
2025-09-11 22:55:41 +0500

    ref: add get connection endpoint

---
+++ src/backend/Pg.Explorer.Features/Connections/GetConnectionById/GetConnectionById.cs
@@ -0,0 +1,54 @@
+﻿using ErrorOr;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pg.Explorer.Domain.Connections;
+using Pg.Explorer.Features.Connections.Shared;
+using Pg.Explorer.Shared.Endpoints.Abstractions;
+using Pg.Explorer.Shared.Endpoints.Extensions;
+
+namespace Pg.Explorer.Features.Connections.GetConnectionById;
+
+public sealed record GetConnectionByIdQuery(long Id) : IRequest<ErrorOr<ConnectionResponse>>;
+
+internal sealed class GetConnectionByIdQueryHandler(IConnectionRepository repository)
+    : IRequestHandler<GetConnectionByIdQuery, ErrorOr<ConnectionResponse>>
+{
+    public async Task<ErrorOr<ConnectionResponse>> Handle(GetConnectionByIdQuery request,
+        CancellationToken cancellationToken)
+    {
+        var entity = await repository.GetByIdAsync(request.Id, cancellationToken);
+        if (entity is null)
+        {
+            return Error.NotFound(code: "Connection.NotFound",
+                description: $"Connection with id {request.Id} not found.");
+        }
+
+        var response = entity.MapToResponse();
+        return response;
+    }
+}
+
+public class GetConnectionByIdEndpoint : IEndpoint
+{
+    public void MapEndpoint(WebApplication app)
+    {
+        app.MapGet("/api/connections/{id:long}", Handle)
+            .WithTags("Connections");
+    }
+
+    private static async Task<IResult> Handle(
+        [FromRoute] long id,
+        IMediator mediator,
+        CancellationToken cancellationToken)
+    {
+        var response = await mediator.Send(new GetConnectionByIdQuery(id), cancellationToken);
+        if (response.IsError)
+        {
+            return response.Errors.ToProblem();
+        }
+
+        return Results.Ok(response.Value);
+    }
+}
