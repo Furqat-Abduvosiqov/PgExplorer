16fc6fb26d82b089d0b5aa924b7087ee2535a8fa
Furqat-Abduvosiqov <furqata93@gmail.com>
2025-09-13 19:00:36 +0500

    feat: add query editor component

---
+++ src/frontend/pg-explorer-client/src/app/features/queries/components/query-editor/query-editor.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-query-editor',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './query-editor.component.html',
+  styleUrl: './query-editor.component.scss'
+})
+export class QueryEditorComponent {
+}
