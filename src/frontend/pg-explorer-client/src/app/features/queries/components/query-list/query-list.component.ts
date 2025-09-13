9ba77c78ed2e2de23dc605f1daaf84f9db1e56a3
Furqat-Abduvosiqov <furqata93@gmail.com>
2025-09-13 18:57:49 +0500

    feat: add query list component

---
+++ src/frontend/pg-explorer-client/src/app/features/queries/components/query-list/query-list.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-query-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './query-list.component.html',
+  styleUrl: './query-list.component.scss'
+})
+export class QueryListComponent {
+}
